edf5abcf71644fa140daa5221f1b507c9e228e87
anmol sharma <anmolsharma433@gmail.com>
2019-11-04 19:41:06 -0500

    Initial Commit

---
+++ CoinFlip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoinFlip
+//
+//  Created by Anmol Sharma on 2019-11-04.
+//  Copyright © 2019 anmol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
